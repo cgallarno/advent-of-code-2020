d9b1b4bf3ff5c4aca00005c605172b32cc780356
Cameron Gallarno <cgallarno@gmail.com>
2020-12-01 01:27:31 -0500

    I don't feel great about it, but it does the job

--- lib/day01.dart
+++ lib/day01.dart
@@ -1,1 +1,27 @@
-int add(int num1, int num2) => num1 + num2;
+int findEntryTotal(List<int> entries, {bool useThreeEntries = false}){
+  if(useThreeEntries){
+    for(var entry in entries){
+      for(var entryTwo in entries){
+        int value = entry + entryTwo;
+        if(value >= 2020){
+          continue;
+        }
+
+        for(var entryThree in entries){
+          if(value + entryThree == 2020){
+            return entry * entryTwo * entryThree;
+          }
+        }
+      }
+    }
+  }else{
+    for(var entry in entries){
+      int matchingValue = entries.firstWhere((otherEntry) => entry + otherEntry == 2020, orElse: (){});
+      if(matchingValue != null){
+        return matchingValue * entry;
+      }
+    }
+  }
+
+  return 0;
+}
--- test/day01_test.dart
+++ test/day01_test.dart
@@ -1,8 +1,32 @@
+import 'dart:io';
+import 'dart:core';
 import 'package:test/test.dart';
 import 'package:aoc2020/day01.dart';
 
+const String dataFilePath = './data/day01.txt';
 void main() {
-  test('1 + 1', (){
-    expect(add(1, 1), equals(2));
+  List<String> data = File(dataFilePath).readAsLinesSync();
+
+  test('Example', (){
+    expect(findEntryTotal([1721, 979, 366, 299, 675, 1456]), equals(514579));
   });
+
+  test('scenario one answer', (){
+    var asIntegers = data.map((s) => int.parse(s));
+    int result = findEntryTotal(asIntegers.toList());
+    print('Your Answer: $result');
+    expect(result, equals(482811));
+  });
+
+  test('scenario two example', (){
+    expect(findEntryTotal([1721, 979, 366, 299, 675, 1456], useThreeEntries: true), equals(241861950));
+  });
+
+  test('scenario two answer', (){
+    var asIntegers = data.map((s) => int.parse(s));
+    int result = findEntryTotal(asIntegers.toList(), useThreeEntries: true);
+    expect(result, isNot(737559));
+    print('Your Answer: $result');
+  });
+
 }
