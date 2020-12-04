dc1733252bea2a07a34f55201d0e337d3d8e36a5
Cameron Gallarno <cgallarno@gmail.com>
2020-12-04 10:24:02 -0500

    Day 4 solution

--- lib/day04.dart
+++ lib/day04.dart
@@ -1,3 +1,181 @@
-int solve(List<String> data) {
-  return 0;
+import 'dart:math';
+
+class Passport {
+  static const List<String> eyeColors = [
+    'amb',
+    'blu',
+    'brn',
+    'gry',
+    'grn',
+    'hzl',
+    'oth'
+  ];
+  static const List<String> requiredFields = [
+    'byr',
+    'iyr',
+    'eyr',
+    'hgt',
+    'hcl',
+    'ecl',
+    'pid'
+  ];
+  //'cid' ]
+
+  int _birthYear;
+  int _issueYear;
+  int _expirationYear;
+  int _heightValue;
+  String _heightUnit;
+  String _hairColor;
+  String _eyeColor;
+  String _passportId;
+  String countryId;
+
+  String get birthYear => _birthYear != null ? _birthYear.toString() : null;
+  void set birthYear(String value) {
+    int year = int.tryParse(value);
+    if (year != null && year >= 1920 && year <= 2002) {
+      _birthYear = year;
+    } else {
+      // print('    value($value) not valid for birthYear');
+    }
+  }
+
+  String get issueYear => _issueYear != null ? _issueYear.toString() : null;
+  void set issueYear(String value) {
+    int year = int.tryParse(value);
+
+    if (year != null && year >= 2010 && year <= 2020) {
+      _issueYear = year;
+    } else {
+      //print('value($value) not valid for issueYear');
+    }
+  }
+
+  String get expirationYear =>
+      _expirationYear != null ? _expirationYear.toString() : null;
+  void set expirationYear(String value) {
+    int year = int.tryParse(value);
+
+    if (year != null && year >= 2020 && year <= 2030) {
+      _expirationYear = year;
+    } else {
+      //print('value($value) not valid for expirationYear');
+    }
+  }
+
+  String get height => _heightValue != null && _heightUnit != null
+      ? '$_heightValue$_heightUnit'
+      : null;
+
+  void set height(String value) {
+    RegExpMatch unitValue = new RegExp(r'[cm|in]$').firstMatch(value);
+    if (unitValue == null) return;
+    String unit = value.substring(unitValue.start - 1, unitValue.end);
+    int parsedValue = int.tryParse(value.substring(0, unitValue.start - 1));
+
+    if (unit != null && parsedValue != null) {
+      bool isValidCm = unit == 'cm' && parsedValue >= 150 && parsedValue <= 193;
+      bool isValidIn = unit == 'in' && parsedValue >= 59 && parsedValue <= 76;
+      if (isValidIn || isValidCm) {
+        _heightUnit = unit;
+        _heightValue = parsedValue;
+      }
+    }
+  }
+
+  String get hairColor => _hairColor;
+  void set hairColor(String value) {
+    RegExp colorPattern = new RegExp(r'^#(?:[0-9a-fA-F]{6})$');
+
+    if (colorPattern.hasMatch(value)) {
+      _hairColor = value;
+    } else {
+      //print('value($value) not valid for hairColor');
+    }
+  }
+
+  String get eyeColor => _eyeColor;
+  void set eyeColor(String value) {
+    if (eyeColors.contains(value)) {
+      _eyeColor = value;
+    } else {
+      //print('value($value) not valid for eyeColor');
+    }
+  }
+
+  String get passportId => _passportId;
+  void set passportId(String value) {
+    RegExp idPattern = new RegExp(r'^[\d]{9}$');
+    if (idPattern.hasMatch(value)) {
+      _passportId = value;
+    } else {
+      //print('value($value) not valid for passportId');
+    }
+  }
+
+  Map<String, String> data;
+
+  Passport(String fields) {
+    List<List<String>> keyValues = fields
+        .trim()
+        .split(' ')
+        .map((keyValue) => keyValue.split(':'))
+        .toList();
+
+    data = Map.fromIterable(keyValues,
+        key: (item) => item[0], value: (item) => item[1]);
+
+    for (var field in requiredFields) {
+      data.putIfAbsent(field, () => '');
+    }
+
+    birthYear = data['byr'];
+    issueYear = data['iyr'];
+    expirationYear = data['eyr'];
+    height = data['hgt'];
+    hairColor = data['hcl'];
+    eyeColor = data['ecl'];
+    passportId = data['pid'];
+    countryId = data['cid'];
+  }
+
+  bool validate() {
+    if (passportId == '012533040') {
+      //print('-> $expirationYear, $eyeColor');
+    }
+    return notNull(birthYear) &&
+        notNull(issueYear) &&
+        notNull(expirationYear) &&
+        notNull(height) &&
+        notNull(hairColor) &&
+        notNull(eyeColor) &&
+        notNull(passportId);
+  }
+
+  bool notNull(value) {
+    // print('checking null for $value');
+    return value != null && value != '' && value != 'null';
+  }
+}
+
+int solve(List<String> passportData) {
+  List<Passport> passports = [];
+
+  String currentPassportData = '';
+  for (var data in passportData) {
+    if (data == '') {
+      passports.add(new Passport(currentPassportData));
+      currentPassportData = '';
+    } else {
+      currentPassportData += ' $data';
+    }
+  }
+
+  passports.add(new Passport(currentPassportData));
+
+  Iterable<Passport> validPassports =
+      passports.where((passport) => passport.validate());
+
+  return validPassports.length;
 }
--- test/day04_test.dart
+++ test/day04_test.dart
@@ -9,30 +9,138 @@ void main() {
   List<String> data = File(dataFilePath).readAsLinesSync();
   // String datcleara = File(dataFilePath).readAsStringSync();
 
-  List<String> sampleData = [];
+  List<String> sampleData = [
+    'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd',
+    'byr:1937 iyr:2017 cid:147 hgt:183cm',
+    '',
+    'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884',
+    'hcl:#cfa07d byr:1929',
+    '',
+    'hcl:#ae17e1 iyr:2013',
+    'eyr:2024',
+    'ecl:brn pid:760753108 byr:1931',
+    'hgt:179cm',
+    '',
+    'hcl:#cfa07d eyr:2025 pid:166559648',
+    'iyr:2011 ecl:brn hgt:59in',
+  ];
 
   group('Scenario One', () {
     test('example', () {
-      expect(solve(sampleData), equals(0));
+      expect(solve(sampleData), equals(2));
     });
     test('solution', () {
       int result = solve(data);
       print('Your Answer: $result');
 
-      expect(result, equals(null));
+      expect(result, equals(170));
+    });
+  });
+
+  group('Passport Model', () {
+    test('birthYear', () {
+      Passport ps1 = new Passport('byr:2002');
+      Passport ps2 = new Passport('byr:2003');
+      expect(ps1.birthYear, equals('2002'));
+      expect(ps2.birthYear, equals(null));
+    });
+
+    test('issueYear', () {
+      Passport ps1 = new Passport('iyr:2010');
+      Passport ps2 = new Passport('iyr:2021');
+      expect(ps1.issueYear, equals('2010'));
+      expect(ps2.issueYear, equals(null));
+    });
+
+    test('expirationYear', () {
+      Passport ps1 = new Passport('eyr:2025');
+      Passport ps2 = new Passport('eyr:2035');
+      expect(ps1.expirationYear, equals('2025'));
+      expect(ps2.expirationYear, equals(null));
+    });
+
+    test('height', () {
+      Passport ps1 = new Passport('hgt:60in');
+      Passport ps2 = new Passport('hgt:190cm');
+
+      Passport ps3 = new Passport('hgt:190in');
+      Passport ps4 = new Passport('hgt:190');
+
+      expect(ps1.height, equals('60in'));
+      expect(ps2.height, equals('190cm'));
+
+      expect(ps3.height, equals(null));
+      expect(ps4.height, equals(null));
+    });
+
+    test('hairColor', () {
+      Passport ps1 = new Passport('hcl:#123abc');
+      Passport ps2 = new Passport('hcl:#123abz');
+      Passport ps3 = new Passport('hcl:123abc');
+
+      expect(ps1.hairColor, equals('#123abc'));
+      expect(ps2.hairColor, equals(null));
+      expect(ps3.hairColor, equals(null));
+    });
+
+    test('eyeColor', () {
+      Passport ps1 = new Passport('ecl:brn');
+      Passport ps2 = new Passport('ecl:wat');
+      expect(ps1.eyeColor, equals('brn'));
+      expect(ps2.eyeColor, equals(null));
+    });
+
+    test('passportId', () {
+      Passport ps1 = new Passport('pid:000000001');
+      Passport ps2 = new Passport('pid:0123456789');
+      expect(ps1.passportId, equals('000000001'));
+      expect(ps2.passportId, equals(null));
     });
   });
 
   group('Scenario Two', () {
-    test('example', () {
-      expect(solve(sampleData), equals(0));
+    List<String> invalidPassports = [
+      'eyr:1972 cid:100',
+      'hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926',
+      '',
+      'iyr:2019',
+      'hcl:#602927 eyr:1976 hgt:170cm',
+      'ecl:grn pid:012533040 byr:1946',
+      '',
+      'hcl:dab227 iyr:2012',
+      'ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277',
+      '',
+      'hgt:59cm ecl:zzz',
+      'eyr:2038 hcl:74454a iyr:2023',
+      'pid:3556412378 byr:2007'
+    ];
+
+    List<String> validPassports = [
+      'pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980',
+      'hcl:#623a2f',
+      '',
+      'eyr:2029 ecl:blu cid:129 byr:1989',
+      'iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm',
+      '',
+      'hcl:#888785',
+      'hgt:164cm byr:2001 iyr:2015 cid:88',
+      'pid:545766238 ecl:hzl',
+      'eyr:2022',
+    ];
+    test('all valid', () {
+      expect(solve(validPassports), equals(3));
+    });
+
+    test('all invalid', () {
+      expect(solve(invalidPassports), equals(0));
     });
 
     test('solution', () {
       int result = solve(data);
       print('Your Answer: $result');
 
-      expect(result, equals(null));
+      expect(result, isNot(111)); // too high
+      expect(result, equals(103));
     });
   });
 }
